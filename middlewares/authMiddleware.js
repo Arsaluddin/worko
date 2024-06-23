a9fa50246ff7115f36b363a0f0a89cc13b37cdb7
arsal uddin <arsaluddin134@gmail.com>
2024-06-23 20:35:20 +0530

    bug resolved

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,18 @@
 module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (authHeader && authHeader.startsWith('Basic ')) {
-      const base64Credentials = authHeader.split(' ')[1];
-      const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-      const [username, password] = credentials.split(':');
-  
-      // Validate username and password
-      if (username === process.env.USERNAME && password === process.env.PASSWORD) {
-        return next();
-      }
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Basic ')) {
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+
+    // Validate username and password
+    if (username === process.env.APP_USERNAME && password === process.env.APP_PASSWORD) {
+      return next();
     }
-    res.status(401).send('Unauthorized');
-  };
+  }
+  res.status(401).send('Unauthorized');
+};
+
+
   
   
